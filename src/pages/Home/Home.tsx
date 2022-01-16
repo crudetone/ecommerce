fbbc19397359f201d1574ffa0c191815e502adf3
Eugeniusz Zujew <eug.zuev@gmail.com>
2022-01-16 18:13:12 +0100

    newsletter

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,7 @@ import Navbar from "../../components/Navbar";
 import React from "react";
 import Slider from "../../components/Slider";
 import Products from "../../components/Products";
+import Newsletter from "../../components/Newsletter";
 
 const Home: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const Home: React.FC = () => {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
     </div>
   );
 };
