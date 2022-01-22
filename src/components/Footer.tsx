b89f22148141dd95b61a160619f3df9bcf7410a3
Eugeniusz Zujew <eug.zuev@gmail.com>
2022-01-22 19:19:56 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,52 +1,130 @@
-import { Facebook, Instagram, Pinterest, Twitter } from "@material-ui/icons";
+import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@material-ui/icons";
 import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
 `;
+
 const Left = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
   padding: 20px;
 `;
+
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+
+const Title = styled.h3`
+margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+margin: 0;
+padding: 0;
+list-style: none;
+display: flex;
+flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+width: 50%;
+margin-bottom: 10px;
+`;
+
 const Logo = styled.h1``;
 
-const Desc = styled.p``;
+const Desc = styled.p`
+  margin: 20px 0;
+`;
 
-const SocialContainer = styled.div``;
+const SocialContainer = styled.div`
+  display: flex;
+`;
 
-const SocialIcon = styled.div``;
+const SocialIcon = styled.div`
+  align-items: center;
+  background-color: #${props => props.color};
+  border-radius: 50%;
+  color: #fff;
+  display: flex;
+  height: 40px;
+  justify-content: center;
+  width: 40px;
+  margin-right: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
 
 const Footer = () => {
   return (
     <Container>
       <Left>
-        <Logo></Logo>
-        <Desc></Desc>
+        <Logo>Devez e-shop</Logo>
+        <Desc>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit culpa
+          voluptatem tenetur. Eaque, impedit! Porro laudantium at temporibus.
+          Repellat aliquam et sapiente maxime, iure quaerat vitae dolorem
+          recusandae animi commodi?
+        </Desc>
         <SocialContainer>
-          <SocialIcon>
+          <SocialIcon color="3B5999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E4405F">
             <Instagram />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="55ACEE">
             <Twitter />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E60023">
             <Pinterest />
           </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Userful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man fashion</ListItem>
+          <ListItem>Woman fasion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{marginRight: "10px"}} />622 Dixie Path. South Tobinchester 98336
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{marginRight: "10px"}} /> +1 234 56 78
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{marginRight: "10px"}}/>contact@email.dev
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
     </Container>
   );
 };
