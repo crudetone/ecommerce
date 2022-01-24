3322f62fcab78127de82bf7269e9721b68f97245
Eugeniusz Zujew <eug.zuev@gmail.com>
2022-01-24 07:04:34 +0100

    product list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import './App.css';
-import Home from './pages/Home/Home';
+import ProductList from './pages/ProductList';
+// import Home from './pages/Home';
 
 const App = () => {
   return (
-    <Home />
+    // <Home />
+    <ProductList />
   );
 }
 
